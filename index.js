3c351368ec78a4c3472e8dd2a94aae243cfcede6
annelysems <annymellosav@hotmail.com>
2024-11-20 15:38:08 -0300

    Desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Olá Mundo")
+let name = "Anne"
+let xp = 19876
+
+if (xp < 1000) {
+   console.log("O Herói de nome " + name + " está no nível de Ferro")
+} else if (xp > 1000 && xp < 2000) {
+    console.log("O Herói de nome " + name + " está no nível de Bronze")
+} else if (xp > 2000 && xp < 5000) {
+    console.log("O Herói de nome " + name + " está no nível de Prata")
+} else if (xp > 5000 && xp < 7000) {
+    console.log("O Herói de nome " + name + " está no nível de Ouro")
+} else if (xp > 7000 && xp < 8000) {
+    console.log("O Herói de nome " + name + " está no nível de Platina")
+} else if (xp > 8000 && xp < 9000) {
+    console.log("O Herói de nome " + name + " está no nível de Ascendente")
+} else if (xp > 9000 && xp < 10000) {
+    console.log("O Herói de nome " + name + " está no nível de Imortal")
+} else {
+    console.log("O Herói de nome " + name + " está no nível de Radiante")
+}
